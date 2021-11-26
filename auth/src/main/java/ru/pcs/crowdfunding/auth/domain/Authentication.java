2e0ba0576df0f73b998c12085c17f8c6581eca65
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-11-26 11:30:25 +0300

    add Authentication entity

---
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/domain/Authentication.java
@@ -0,0 +1,40 @@
+package ru.pcs.crowdfunding.auth.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString()
+@EqualsAndHashCode()
+@Entity
+@Table(name = "authentication")
+public class Authentication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "access_token")
+    @Lob
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    @Lob
+    private String refreshToken;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+}
