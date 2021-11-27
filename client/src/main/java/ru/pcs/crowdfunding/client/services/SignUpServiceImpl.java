b62445641adc5813c49aa0adad7632d841b75400
Marsel Zakirov <musictest666@yandex.ru>
2021-11-27 17:47:48 +0300

    Добавил SignInDto,SignUpDto и Validation и контроллер signUp.

---
+++ client/src/main/java/ru/pcs/crowdfunding/client/services/SignUpServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.pcs.crowdfunding.client.services;
+
+import ru.pcs.crowdfunding.client.dto.SignUpForm;
+
+import java.util.Locale;
+
+public class SignUpServiceImpl implements SignUpService {
+    private final ClientsRepository clientsRepository;
+
+    @Override
+    public void signUp(SignUpForm form) {
+         Client client = Client.builder()
+                .firstName(form.getFirstName())
+                .lastName(form.getLastName())
+                .country(form.country())
+                .city(form.city())
+                .email(form.getEmail().toLowerCase(Locale.ROOT))
+                .password(form.getPassword())
+                .build();
+
+        clientsRepository.save(client);
+    }
+}
