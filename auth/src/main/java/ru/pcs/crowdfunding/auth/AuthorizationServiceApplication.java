49d41bccbcf8512f6f963260ae7a03e4d0ed5032
Nikolai Ponomarev <isaveryan@mail.ru>
2021-11-27 18:09:46 +0200

    fixed : deleted useless changes

--- auth/src/main/java/ru/pcs/crowdfunding/auth/AuthorizationServiceApplication.java
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/AuthorizationServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthorizationServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuthorizationServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServiceApplication.class, args);
+	}
 
 }
