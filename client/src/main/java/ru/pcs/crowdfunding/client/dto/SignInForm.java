db57f3e254018b6081e8dfa3b7b3917cff579c8e
Marsel Zakirov <musictest666@yandex.ru>
2021-11-27 17:09:55 +0300

    Добавил SignInDto,SignUpDto и Validation

---
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/SignInForm.java
@@ -0,0 +1,17 @@
+package ru.pcs.crowdfunding.client.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInForm {
+
+    private String email;
+
+    private String password;
+}
---
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/SignUpForm.java
@@ -0,0 +1,44 @@
+package ru.pcs.crowdfunding.client.dto;
+
+import lombok.*;
+import ru.pcs.crowdfunding.client.validation.NotSameNames;
+import ru.pcs.crowdfunding.client.validation.ValidPassword;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@NotSameNames
+public class SignUpForm {
+
+    @Size(min = 4, max = 20)
+    @NotBlank
+    private String firstName;
+
+    @Size(min = 4, max = 20)
+    @NotBlank
+    private String lastName;
+
+    @Size(min = 3, max = 20)
+    @NotBlank
+    private String country;
+
+    @Size(min = 2, max = 20)
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    @Size(min = 7, max = 20)
+    @ValidPassword
+    private String password;
+
+    @Email
+    @NotBlank
+    private String email;
+
+}
