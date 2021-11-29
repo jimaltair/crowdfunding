efb6b7ba10b8216e14d1ab12173081961b5af17a
Nikolai Ponomarev <isaveryan@mail.ru>
2021-11-29 19:53:24 +0200

    Добавил обработку запросов на создание проекта

---
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/ProjectForm.java
@@ -0,0 +1,36 @@
+package ru.pcs.crowdfunding.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.pcs.crowdfunding.client.domain.ProjectImage;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.awt.*;
+
+/**
+ * 28.11.2021
+ * Crowdfunding
+ *
+ * @author Nikolay Ponomarev
+ * @version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProjectForm {
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+
+    private ProjectImage image;
+
+}
--- client/src/main/java/ru/pcs/crowdfunding/client/services/ProjectsServiceImpl.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/services/ProjectsServiceImpl.java
@@ -2,12 +2,27 @@ package ru.pcs.crowdfunding.client.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.pcs.crowdfunding.client.domain.Client;
 import ru.pcs.crowdfunding.client.domain.Project;
+import ru.pcs.crowdfunding.client.domain.ProjectImage;
+import ru.pcs.crowdfunding.client.domain.ProjectStatus;
 import ru.pcs.crowdfunding.client.dto.ProjectDto;
+import ru.pcs.crowdfunding.client.dto.ProjectForm;
+import ru.pcs.crowdfunding.client.repositories.ClientsRepository;
+import ru.pcs.crowdfunding.client.repositories.ProjectImagesRepository;
+import ru.pcs.crowdfunding.client.repositories.ProjectStatusesRepository;
 import ru.pcs.crowdfunding.client.repositories.ProjectsRepository;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.Optional;
+import java.util.UUID;
 
 import static ru.pcs.crowdfunding.client.dto.ProjectDto.from;
 
@@ -18,6 +33,12 @@ public class ProjectsServiceImpl implements ProjectsService {
 
     private final ProjectsRepository projectsRepository;
 
+    private final ClientsRepository clientsRepository;
+
+    private final ProjectStatusesRepository projectStatusesRepository;
+
+    private final ProjectImagesRepository projectImagesRepository;
+
     @Override
     public Optional<ProjectDto> findById(Long id) {
         Optional<Project> project = projectsRepository.findById(id);
@@ -26,5 +47,38 @@ public class ProjectsServiceImpl implements ProjectsService {
             return Optional.empty();
         }
         return Optional.of(from(project.get()));
+    }
+
+    @Override
+    public void createProject(ProjectForm form, MultipartFile file) {
+
+        Project project = Project.builder()
+                .author(clientsRepository.getById(2L))
+                .title(form.getTitle())
+                .description(form.getDescription())
+                .createdAt(Instant.now())
+                .finishDate(Instant.now())
+                .moneyGoal(BigDecimal.ZERO)
+                .status(projectStatusesRepository.getById(1L))
+                .build();
+
+
+        ProjectImage image = ProjectImage.builder()
+                .project(project)
+                .path("C:\\Users\\isave\\Desktop\\ponomarev_nikolay_java_pcs_21_01_homeworks\\Crowdfunding\\client\\src\\main\\resources\\static\\upload"
+                        + UUID.randomUUID() + file.getOriginalFilename())
+                .build();
+
+
+        try {
+            log.debug(image.getPath());
+            log.info(image.getPath());
+            Files.copy(file.getInputStream(), Paths.get(image.getPath()));
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        projectsRepository.save(project);
+        projectImagesRepository.save(image);
     }
 }
