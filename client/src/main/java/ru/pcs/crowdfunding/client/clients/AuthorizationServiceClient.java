a967bc70c526ead16fcaced4ab78f7cf97d67ef4
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-11-30 19:06:07 +0300

    add signUp() method to AuthorizationServiceClient

--- client/src/main/java/ru/pcs/crowdfunding/client/clients/AuthorizationServiceClient.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/clients/AuthorizationServiceClient.java
@@ -1,7 +1,10 @@
 package ru.pcs.crowdfunding.client.clients;
 
 import org.springframework.http.ResponseEntity;
+import ru.pcs.crowdfunding.client.dto.AuthSignUpRequest;
 
 public interface AuthorizationServiceClient {
     ResponseEntity<String> ping();
+
+    ResponseEntity<Void> signUp(AuthSignUpRequest request);
 }
--- client/src/main/java/ru/pcs/crowdfunding/client/clients/AuthorizationServiceRestTemplateClient.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/clients/AuthorizationServiceRestTemplateClient.java
@@ -6,10 +6,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import ru.pcs.crowdfunding.client.dto.AuthSignUpRequest;
 
 @Component
 public class AuthorizationServiceRestTemplateClient implements AuthorizationServiceClient {
     private static final String PING_URL = "/api/v0/ping";
+    private static final String SIGNUP_URL = "/signUp";
 
     private final RestTemplate restTemplate;
     private final String remoteAddress;
@@ -25,5 +27,10 @@ public class AuthorizationServiceRestTemplateClient implements AuthorizationServ
     @Override
     public ResponseEntity<String> ping() {
         return restTemplate.getForEntity(remoteAddress + PING_URL, String.class);
+    }
+
+    @Override
+    public ResponseEntity<Void> signUp(AuthSignUpRequest request) {
+        return restTemplate.postForEntity(remoteAddress + SIGNUP_URL, request, Void.class);
     }
 }
