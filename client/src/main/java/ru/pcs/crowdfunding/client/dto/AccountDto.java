05a7c6249e9f35f72e2a18a10f3cd925771a3330
Svetlana Astapova <morgan159@mail.ru>
2021-12-02 02:34:50 +0500

    delete unused import

--- client/src/main/java/ru/pcs/crowdfunding/client/dto/AccountDto.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/AccountDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.pcs.crowdfunding.client.domain.Client;
 
 import java.time.Instant;
 
