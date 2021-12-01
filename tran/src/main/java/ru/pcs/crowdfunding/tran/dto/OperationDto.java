ef261a49ab860e4ecce1da09ead0f62152ac91bb
Svetlana Astapova <morgan159@mail.ru>
2021-12-02 02:41:29 +0500

    change OperationDto

--- tran/src/main/java/ru/pcs/crowdfunding/tran/dto/OperationDto.java
+++ tran/src/main/java/ru/pcs/crowdfunding/tran/dto/OperationDto.java
@@ -1,39 +1,22 @@
 package ru.pcs.crowdfunding.tran.dto;
 
 import lombok.*;
-import ru.pcs.crowdfunding.tran.domain.Account;
-import ru.pcs.crowdfunding.tran.domain.Operation;
-import ru.pcs.crowdfunding.tran.domain.OperationType;
-import ru.pcs.crowdfunding.tran.domain.Payment;
-
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OperationDto {
-    private Long id;
-    private Long initiator;
+    private Long initiatorId;
     private Instant datetime;
-    private OperationType operationType;
-    private Account debitAccount;
-    private Account creditAccount;
+    private String operationType;
+    private Long debitAccountId;
+    private Long creditAccountId;
     private BigDecimal sum;
-    private List<Payment> payments;
 
-    public static OperationDto from(Operation operation) {
-        return OperationDto.builder()
-                .id(operation.getId())
-                .initiator(operation.getInitiator())
-                .datetime(operation.getDatetime())
-                .operationType(operation.getOperationType())
-                .debitAccount(operation.getDebitAccount())
-                .creditAccount(operation.getCreditAccount())
-                .sum(operation.getSum())
-                .payments(operation.getPayments())
-                .build();
+    public static OperationDto from (Operation operation) {
+        return new OperationDto(); //временно
     }
 }
