71b63329e5f98a271e2eaab36cf1c7ae7e96fc90
Svetlana Astapova <morgan159@mail.ru>
2021-12-01 19:58:39 +0500

    add fields

--- client/src/main/java/ru/pcs/crowdfunding/client/dto/OperationDto.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/OperationDto.java
@@ -3,13 +3,21 @@ package ru.pcs.crowdfunding.client.dto;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OperationDto {
-    Long userId;
-    BigDecimal sumOperation;
-    Long accountIdToTransfer;
+    private Long id;
+    private Long clientId;
+    private Instant datetime;
+    private OperationType operationType;
+    private Account debitAccount;
+    private Account creditAccount;
+    private BigDecimal sum;
+    private List<Payment> payments;
 }
+
+
