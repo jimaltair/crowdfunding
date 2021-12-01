6a2bb8d0dc3ab0a55f10727c416030edd908cb2e
Svetlana Astapova <morgan159@mail.ru>
2021-12-01 20:40:26 +0500

    change AccountDto (add method from)

--- client/src/main/java/ru/pcs/crowdfunding/client/dto/AccountDto.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/dto/AccountDto.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.pcs.crowdfunding.client.domain.Client;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class AccountDto {
-    Long accountId;
+    private Long accountId;
+
+    public static AccountDto from(Client client) {
+        return AccountDto.builder()
+                .accountId(client.getAccountId())
+                .build();
+    }
 }
