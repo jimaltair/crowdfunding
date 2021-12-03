d89987a02aa0b0f9bf45a89fc228421626c05348
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-12-03 12:45:58 +0300

    call authorization service on signup

--- client/src/main/java/ru/pcs/crowdfunding/client/services/SignUpServiceImpl.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/services/SignUpServiceImpl.java
@@ -1,8 +1,12 @@
 package ru.pcs.crowdfunding.client.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import ru.pcs.crowdfunding.client.api.AuthorizationServiceClient;
 import ru.pcs.crowdfunding.client.domain.Client;
+import ru.pcs.crowdfunding.client.dto.AuthSignUpRequest;
+import ru.pcs.crowdfunding.client.dto.ResponseDto;
 import ru.pcs.crowdfunding.client.dto.SignUpForm;
 import ru.pcs.crowdfunding.client.repositories.ClientsRepository;
 
@@ -11,6 +15,7 @@ import ru.pcs.crowdfunding.client.repositories.ClientsRepository;
 @RequiredArgsConstructor
 public class SignUpServiceImpl implements SignUpService {
     private final ClientsRepository clientsRepository;
+    private final AuthorizationServiceClient authorizationServiceClient;
 
     @Override
     public SignUpForm signUp(SignUpForm form) {
@@ -21,7 +26,44 @@ public class SignUpServiceImpl implements SignUpService {
                 .city(form.getCity())
                 .build();
 
-        clientsRepository.save(newClient);
+        // сохраняем заготовку клиента локально
+        newClient = clientsRepository.save(newClient);
+
+        // ходим в остальные сервисы и обновляем поля клиента
+        newClient = callAuthorizationService(form, newClient);
+        newClient = callTransactionService(newClient);
+
+        // сохраняем дополненного клиента
+        newClient = clientsRepository.save(newClient);
+
         return SignUpForm.from(newClient);
+    }
+
+    private Client callAuthorizationService(SignUpForm form, Client client) {
+        AuthSignUpRequest authSignUpRequest = AuthSignUpRequest.builder()
+                .userId(client.getId())
+                .email(form.getEmail())
+                .password(form.getPassword())
+                .build();
+
+        ResponseEntity<ResponseDto> responseEntity = authorizationServiceClient.signUp(authSignUpRequest);
+        if (responseEntity.getStatusCode().isError()) {
+            // FIXME: handle this properly
+            throw new IllegalStateException("AuthorizationServiceClient.signUp() failed");
+        }
+
+        ResponseDto responseDto = responseEntity.getBody();
+        if (!responseDto.isSuccess()) {
+            // FIXME: handle this properly
+            throw new IllegalStateException("AuthorizationServiceClient.signUp() failed");
+        }
+
+        return client;
+    }
+
+    private Client callTransactionService(Client client) {
+        // TODO: create account in transaction service
+
+        return client;
     }
 }
