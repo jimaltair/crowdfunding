c98850e79ad28a8759a462a3d18428d1a52c1e63
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-12-06 13:50:55 +0300

    rollback OperationServiceImpl to dev version

--- tran/src/main/java/ru/pcs/crowdfunding/tran/services/OperationServiceImpl.java
+++ tran/src/main/java/ru/pcs/crowdfunding/tran/services/OperationServiceImpl.java
@@ -2,13 +2,17 @@ package ru.pcs.crowdfunding.tran.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.pcs.crowdfunding.tran.domain.Operation;
 import ru.pcs.crowdfunding.tran.domain.OperationType;
 import ru.pcs.crowdfunding.tran.domain.Payment;
 import ru.pcs.crowdfunding.tran.dto.OperationDto;
 import ru.pcs.crowdfunding.tran.repositories.AccountsRepository;
+import ru.pcs.crowdfunding.tran.repositories.OperationTypesRepository;
 import ru.pcs.crowdfunding.tran.repositories.OperationsRepository;
 import ru.pcs.crowdfunding.tran.repositories.PaymentsRepository;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,38 +21,42 @@ public class OperationServiceImpl implements OperationService {
     private final PaymentsRepository paymentsRepository;
     private final AccountsRepository accountsRepository;
     private final OperationsRepository operationsRepository;
+    private final OperationTypesRepository operationTypesRepository;
     private final OperationValidator operationValidator;
 
+    //region private methods
+    private Operation operationBuilder(OperationDto operationDto) {
+        Operation operation = Operation.builder()
+            .initiator(operationDto.getInitiatorId())
+            .datetime(operationDto.getDatetime())
+            .sum(operationDto.getSum())
+            .operationType(operationTypesRepository.findOperationType(operationDto.getOperationType()))
+            .build();
+        return operation;
+    }
+
+    private Payment replenishmentTransactionBuilder(OperationDto operationDto, Operation operation) {
+        return Payment.builder()
+            .account(accountsRepository.getById(operationDto.getDebitAccountId()))
+            .sum(operationDto.getSum())
+            .operation(operation)
+            .datetime(operationDto.getDatetime())
+            .build();
+    }
+
+    private Payment writeOffTransactionBuilder(OperationDto operationDto, Operation operation) {
+        return Payment.builder()
+            .account(accountsRepository.getById(operationDto.getCreditAccountId()))
+            .sum(operationDto.getSum().multiply(BigDecimal.valueOf(-1)))
+            .operation(operation)
+            .datetime(operationDto.getDatetime())
+            .build();
+    }
+    //endregion
+
     @Override
-    public OperationDto createOperation(OperationDto operationDto) {
-
-
-        operationValidator.isValid(operationDto); // пока просто вызываю, ничего не обрабатываю; исключение должно проброситься сюда
-
-
-        if (operationDto.getOperationType().equals(OperationType.Type.PAYMENT.toString()) ||
-                operationDto.getOperationType().equals(OperationType.Type.REFUND.toString()) ||
-                operationDto.getOperationType().equals(OperationType.Type.TOP_UP.toString())) {
-
-            paymentsRepository.save(Payment.builder()
-                    .account(accountsRepository.getById(operationDto.getDebitAccountId()))
-                    .sum(operationDto.getSum())
-                    .operation(operationsRepository.getById(operationDto.getId()))
-                    .datetime(operationDto.getDatetime())
-                    .build());
-
-        }
-
-        if (operationDto.getOperationType().equals(OperationType.Type.PAYMENT.toString()) ||
-                operationDto.getOperationType().equals(OperationType.Type.REFUND.toString()) ||
-                operationDto.getOperationType().equals(OperationType.Type.WITHDRAW.toString())) {
-
-            paymentsRepository.save(Payment.builder()
-                    .account(accountsRepository.getById(operationDto.getCreditAccountId()))
-                    .sum(operationDto.getSum().multiply(BigDecimal.valueOf(-1)))
-                    .operation(operationsRepository.getById(operationDto.getId()))
-                    .datetime(operationDto.getDatetime())
-                    .build());
+    @Transactional
+    public OperationDto createOperation(OperationDto operationDto) throws IllegalArgumentException {
 
         operationValidator.isValid(operationDto);
         String operationType = operationDto.getOperationType();
@@ -79,6 +87,13 @@ public class OperationServiceImpl implements OperationService {
             operation = operationsRepository.save(operationBuild);
             paymentsRepository.save(writeOffTransactionBuilder(operationDto, operation));
         }
-        return operationDto;
+            return operationDto;
+    }
+
+    @Override
+    public Optional<OperationDto> findById(Long id) {
+        Optional<Operation> optionalOperation = operationsRepository.findById(id);
+        return optionalOperation.map(OperationDto::from);
     }
 }
+
