3c6fd2d06acf75501a12d1649c932a9149ad4027
Nikolai Ponomarev <isaveryan@mail.ru>
2021-12-06 13:36:42 +0200

    Внесены некоторые исправления

--- auth/src/main/java/ru/pcs/crowdfunding/auth/controllers/AuthController.java
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/controllers/AuthController.java
@@ -25,30 +25,26 @@ public class AuthController {
     public ResponseEntity<ResponseDto> getById(@PathVariable("id") Long id) {
 
         log.info("Запускается метод 'getById' с параметром 'id' - {}", id);
-
         boolean success = true;
         HttpStatus status = HttpStatus.ACCEPTED;
         String errorMessage = null;
         Optional<AuthenticationInfoDto> authenticationInfo = authService.findById(id);
-
         log.info("Создан 'AuthenticationInfoDto' - {} с запуском 'authService'", authenticationInfo);
 
         if (!authenticationInfo.isPresent()) {
 
             log.warn("Не удалось создать 'AuthenticationInfoDto'" +
                     ", так как 'AuthenticationInfo' с id - {} не существует", id);
-
             success = false;
             status = HttpStatus.NOT_FOUND;
+            log.warn("Создан новый 'HttpStatus' - {}", status);
             errorMessage = "Client with id " + id + " not found";
-
         }
 
         ResponseDto response = ResponseDto.buildResponse(success, errorMessage, authenticationInfo);
-
-        log.info("Создан новый 'ResponseDto  - {} с 'isSuccess' - {}", response.getData(), response.isSuccess());
+        log.info("Создан новый 'ResponseDto c содержимым 'data' - {}, 'error' - {}, 'isSuccess' - {}"
+                , response.getData(),response.getError(), response.isSuccess());
         log.info("Завершен метод 'getById' с параметром 'id' - {}", id);
-
         return ResponseEntity.status(status).body(response);
     }
 
@@ -71,77 +67,71 @@ public class AuthController {
             return responseBody;
         }
         ResponseDto response = ResponseDto.buildResponse(true, null, authenticationInfoDto);
+        log.info("Создан новый 'ResponseDto c содержимым 'data' - {}, 'error' - {}, 'isSuccess' - {}"
+                , response.getData(),response.getError(), response.isSuccess());
 
-        log.info("Создан новый 'ResponseDto' - {} с 'isSuccess - {}", response.getData(), response.isSuccess());
-        log.info("Завершается метод 'createAuthenticationInfo' с параметром 'AuthenticationInfoDto' - {}", authenticationInfo);
-
+        log.info("Завершается метод 'createAuthenticationInfo' с параметром 'authenticationInfo' - {}", authenticationInfo);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<ResponseDto> updateAuthenticationInfo(@PathVariable("id") Long id,
                                                                            @RequestBody AuthenticationInfoDto authenticationInfo) {
-
         log.info("Запускается метод 'updateAuthenticationInfo'" +
-                " с параметрами 'id' - {} и 'AuthenticationInfoDto' - {}", id, authenticationInfo);
+                " с параметрами 'id' - {} и 'authenticationInfo' - {}", id, authenticationInfo);
 
         boolean success = true;
         HttpStatus status = HttpStatus.ACCEPTED;
+        log.info("Создан новый 'HttpStatus' - {}", status);
         String errorMessage = null;
         Optional<AuthenticationInfoDto> authenticationInfoDto = authService.updateAuthenticationInfo(id, authenticationInfo);
-
         log.info("Попытка обновить 'authenticationInfo' с 'AuthenticationInfoDto' - {} с запуском 'authService'", authenticationInfoDto);
 
         if (!authenticationInfoDto.isPresent()) {
-
             log.warn("Не удалось создать 'AuthenticationInfoDto'"  +
                                         ", так как 'AuthenticationInfo' с id - {} не существует", id);
-
             success = false;
             status = HttpStatus.NOT_FOUND;
+            log.warn("'HttpStatus' изменен - {}", status);
             errorMessage = "Can't update. Client with id " + id + " not found";
         }
 
         log.info("Успешно");
-
         ResponseDto response = ResponseDto.buildResponse(success, errorMessage, authenticationInfo);
+        log.info("Создан новый 'ResponseDto c содержимым 'data' - {}, 'error' - {}, 'isSuccess' - {}"
+                , response.getData(),response.getError(), response.isSuccess());
 
-        log.info("Создан новый 'ResponseDto' - {} с 'isSuccess' - {}", response.getData(), response.isSuccess());
         log.info("Завершается метод 'updateAuthenticationInfo'" +
-                " с параметрами 'id' - {} и 'AuthenticationInfoDto' - {}", id, authenticationInfo);
-
+                " с параметрами 'id' - {} и 'authenticationInfo' - {}", id, authenticationInfo);
         return ResponseEntity.status(status).body(response);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<ResponseDto> deleteAuthenticationInfo(@PathVariable("id") Long id) {
-
         log.info("Запускается метод 'deleteAuthenticationInfo' с параметром 'id' - {}", id);
 
         boolean success = true;
         HttpStatus status = HttpStatus.ACCEPTED;
+        log.info("Создан новый 'HttpStatus' - {}", status);
         String errorMessage = null;
         Optional<AuthenticationInfoDto> authenticationInfoDto = authService.deleteAuthenticationInfo(id);
-
         log.info("Попытка деактивировать 'authenticationInfo' c 'AuthenticationInfoDto' - {} с запуском 'authService'", authenticationInfoDto);
 
         if (!authenticationInfoDto.isPresent()) {
-
             log.warn("Не удалось создать 'AuthenticationInfoDto'"  +
                     ", так как 'AuthenticationInfo' с id - {} не существует", id);
-
             success = false;
             status = HttpStatus.NOT_FOUND;
+            log.warn("'HttpStatus' изменен - {}", status);
             errorMessage = "Can't delete. Client with id " + id + " not found";
         }
 
         log.info("Успешно");
-
         ResponseDto response = ResponseDto.buildResponse(success, errorMessage, authenticationInfoDto);
+        log.info("Создан новый 'ResponseDto c содержимым 'data' - {}, 'error' - {}, 'isSuccess' - {}"
+                , response.getData(),response.getError(), response.isSuccess());
 
-        log.info("Создан новый 'ResponseDto' - {} с 'isSuccess' - {}", response.getData(), response.isSuccess());
         log.info("Завершается метод 'deleteAuthenticationInfo' с параметром 'id' - {}", id);
-
         return ResponseEntity.status(status).body(response);
     }
 }
