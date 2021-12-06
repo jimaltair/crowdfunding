753f51c6310cab5bb884a7dd23e0b64eb18bee23
Marsel Zakirov <innomars@yandex.ru>
2021-12-06 10:24:47 +0000

    Update AuthControllerMockMvcTest.java

--- auth/src/test/java/ru/pcs/crowdfunding/auth/controllers/AuthControllerMockMvcTest.java
+++ auth/src/test/java/ru/pcs/crowdfunding/auth/controllers/AuthControllerMockMvcTest.java
@@ -37,7 +37,7 @@ class AuthControllerMockMvcTest {
                 .userId(1L)
                 .email("email@email.com")
                 .password("test_pass")
-//                .accessToken("access_test_token") реализация поменялась
+                .accessToken("access_test_token") 
                 .refreshToken("refresh_test_token")
                 .isActive(true)
                 .build());
