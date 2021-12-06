8dcc6f9435b32f4204a2c185a8385ca803834d35
Marsel Zakirov <musictest666@yandex.ru>
2021-12-06 10:45:38 +0300

    Не уверен что всё заметил, пока так.

--- auth/src/main/java/ru/pcs/crowdfunding/auth/dto/AuthorizationInfoDto.java
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/dto/AuthorizationInfoDto.java
@@ -8,6 +8,10 @@ import ru.pcs.crowdfunding.auth.domain.AuthorizationInfo;
 import ru.pcs.crowdfunding.auth.domain.Role;
 import ru.pcs.crowdfunding.auth.domain.Status;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,12 +24,18 @@ public class AuthorizationInfoDto {
     private Status.StatusEnum status;
     private String accessToken;
 
-    public static AuthorizationInfoDto from(AuthorizationInfo authorizationInfo, Role role, Status status) {
+    public static AuthorizationInfoDto from(AuthorizationInfo authorizationInfo) {
         return AuthorizationInfoDto.builder()
                 .userId(authorizationInfo.getUserId())
-                .role(role.getName())
-                .status(status.getName())
+                .role(Role.RoleEnum.USER)
+                .status(Status.StatusEnum.CONFIRMED)
                 .accessToken(authorizationInfo.getAccessToken())
                 .build();
+    }
+
+    public static List<AuthorizationInfoDto> from(Collection<AuthorizationInfo> authorizationInfo) {
+        return authorizationInfo.stream()
+                .map(AuthorizationInfoDto::from)
+                .collect(Collectors.toList());
     }
 }
