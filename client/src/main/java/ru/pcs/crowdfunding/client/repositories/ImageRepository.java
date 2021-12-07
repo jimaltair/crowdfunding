658f71140adce594c6716a004f7918bc4b48ea5a
Mitskevich Igor <jimaltair1987@gmail.com>
2021-12-07 12:02:26 +0200

    add ImageRepository

---
+++ client/src/main/java/ru/pcs/crowdfunding/client/repositories/ImageRepository.java
@@ -0,0 +1,16 @@
+package ru.pcs.crowdfunding.client.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.pcs.crowdfunding.client.domain.Image;
+
+/**
+ * 07.12.2021
+ * client
+ *
+ * @author Mitskevich Igor
+ * @version v1.0
+ */
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
