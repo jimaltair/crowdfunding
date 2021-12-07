bc01cc8a8c03b767554b42b4535657de61efef2f
Ivan-Salnikov <fin.vah@gmail.com>
2021-12-07 21:09:23 +0300

    Remove useless import from RestTemplateClient

--- client/src/main/java/ru/pcs/crowdfunding/client/api/RestTemplateClient.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/api/RestTemplateClient.java
@@ -1,7 +1,6 @@
 package ru.pcs.crowdfunding.client.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.beans.binding.ObjectExpression;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
