98da8098a6bdd818a1a852809a2f549e45125baf
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-12-07 20:41:28 +0300

    save jwt secret key in application.properties

--- auth/src/main/java/ru/pcs/crowdfunding/auth/security/util/JwtTokenProvider.java
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/security/util/JwtTokenProvider.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -14,20 +15,21 @@ import java.util.Date;
 @Slf4j
 public class JwtTokenProvider implements TokenProvider {
 
-    private final static String JWT_SECRET_KEY = "jwt_secret_key";
+    @Value("${security.jwt_secret_key}")
+    private String jwtSecretKey;
 
     @Override
     public String generate(TokenContent tokenContent, Duration lifeTime) {
         return JWT.create()
                 .withExpiresAt(calcExpirationDate(lifeTime))
                 .withSubject(tokenContent.getUserId().toString())
-                .sign(Algorithm.HMAC256(JWT_SECRET_KEY));
+                .sign(Algorithm.HMAC256(jwtSecretKey));
     }
 
     @Override
     public TokenContent decode(String token) throws Exception {
         try {
-            DecodedJWT jwt = JWT.require(Algorithm.HMAC256(JWT_SECRET_KEY))
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC256(jwtSecretKey))
                     .build()
                     .verify(token);
 
