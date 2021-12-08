9ba411a1dd1974d070cf22c15adecae000205fc6
Svetlana Astapova <morgan159@mail.ru>
2021-12-09 00:44:53 +0500

    added SignInController in 'auth' (empty)

---
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/controllers/SignInController.java
@@ -0,0 +1,13 @@
+package ru.pcs.crowdfunding.auth.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/signIn")
+@RequiredArgsConstructor
+@Slf4j
+public class SignInController {
+}
