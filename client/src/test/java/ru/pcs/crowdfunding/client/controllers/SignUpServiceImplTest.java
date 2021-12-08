0b283fa8b7b4f48f08f43ebb694c812d96f264c6
Ivan-Salnikov <fin.vah@gmail.com>
2021-12-08 12:02:38 +0300

    Added test for POST method

--- client/src/test/java/ru/pcs/crowdfunding/client/controllers/SignUpServiceImplTest.java
+++ client/src/test/java/ru/pcs/crowdfunding/client/controllers/SignUpServiceImplTest.java
@@ -3,8 +3,6 @@ package ru.pcs.crowdfunding.client.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.pcs.crowdfunding.client.api.AuthorizationServiceClient;
