33e84fa3c51da9a791e4b7a1f744639b2c7b0828
Nikolai Ponomarev <isaveryan@mail.ru>
2021-12-09 14:15:11 +0200

    Added list of projects in profile_page

--- client/src/main/java/ru/pcs/crowdfunding/client/controllers/ClientController.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/controllers/ClientController.java
@@ -2,25 +2,17 @@ package ru.pcs.crowdfunding.client.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
-import ru.pcs.crowdfunding.client.domain.Project;
 import ru.pcs.crowdfunding.client.dto.ClientDto;
-import ru.pcs.crowdfunding.client.dto.ClientForm;
-import ru.pcs.crowdfunding.client.dto.ImageDto;
 import ru.pcs.crowdfunding.client.services.ClientsService;
-import ru.pcs.crowdfunding.client.services.ProjectsService;
 
-import javax.validation.Valid;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/clients")
@@ -29,63 +21,21 @@ import java.util.stream.Collectors;
 public class ClientController {
 
     private final ClientsService clientsService;
-    private final ProjectsService projectsService;
 
     @GetMapping(value = "/{id}")
     public String getById(@PathVariable Long id, Model model) {
         log.info("get by id = {}", id);
 
-        Optional<ClientDto> client = clientsService.findById(id);
+        Optional<ClientDto> client = clientsService.getById(id);
         if (!client.isPresent()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client with id " + id + " not found");
         }
-        List<Project> projects = client.get().getProjects();
-        List<ProjectDto> projectDtos = projects.stream().map(project -> {
-            Optional<ProjectDto> projectDto = projectsService.findById(project.getId());
-            if (!projectDto.isPresent()) {
-                log.error("Project didn't found");
-                throw new IllegalArgumentException("Project didn't found");
-            }
-            return projectDto.get();}).collect(Collectors.toList());
         log.debug("result = {}", client.get());
 
         model.addAttribute("clientDto", client.get());
-        model.addAttribute("projectsDtos", projectDtos);
 
         return "profile_page";
     }
 
-    @PostMapping(value = "/{id}")
-    public String update(@PathVariable Long id, @Valid ClientForm form, Model model,
-                         @RequestParam("file") MultipartFile file) {
-        log.info("update by id = {}", id);
-
-        ClientForm newForm = clientsService.updateClient(id, form, file);
-
-        model.addAttribute("clientDto", newForm);
-
-        return "redirect:/clients/" + id;
-    }
-
-    @GetMapping("/image/{id}")
-    public ResponseEntity<byte[]> getImageById(@PathVariable("id") Long id) {
-        log.info("get client image by id {}", id);
-
-        Optional<ImageDto> imageDto = clientsService.getImageById(id);
-        if (!imageDto.isPresent()) {
-            log.error("client image with id {} not found", id);
-            return ResponseEntity.notFound().build();
-        }
-
-        log.info("found client image with id {}, format {} and content length {}",
-                id, imageDto.get().getFormat(), imageDto.get().getContent().length);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.valueOf("image/" + imageDto.get().getFormat()));
-        headers.setContentLength(imageDto.get().getContent().length);
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(imageDto.get().getContent());
-    }
 
 }
