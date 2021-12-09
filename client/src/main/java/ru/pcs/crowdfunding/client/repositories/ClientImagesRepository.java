ec280eacb7b87b76f9c83cf32df60a7b24407231
Renat Shaymagsumov <shaymagsumov@gmail.com>
2021-12-09 16:37:31 +0300

    cleanup

--- client/src/main/java/ru/pcs/crowdfunding/client/repositories/ClientImagesRepository.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/repositories/ClientImagesRepository.java
@@ -1,11 +1,7 @@
 package ru.pcs.crowdfunding.client.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.pcs.crowdfunding.client.domain.ClientImage;
 
 public interface ClientImagesRepository extends JpaRepository<ClientImage, Long> {
-
-    @Query(value = "select * from client_image where client_id = ?1", nativeQuery = true)
-    byte[] getBytesImage(Long clientId);
 }
