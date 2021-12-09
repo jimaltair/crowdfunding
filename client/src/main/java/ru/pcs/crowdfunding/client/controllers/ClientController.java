00e96e00be42761e348506037953159d2a0374e9
Nikolai Ponomarev <isaveryan@mail.ru>
2021-12-09 21:35:11 +0200

    fixed some conflicts

--- client/src/main/java/ru/pcs/crowdfunding/client/controllers/ClientController.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/controllers/ClientController.java
@@ -11,16 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-import ru.pcs.crowdfunding.client.domain.Project;
 import ru.pcs.crowdfunding.client.dto.ClientDto;
 import ru.pcs.crowdfunding.client.dto.ClientForm;
 import ru.pcs.crowdfunding.client.dto.ImageDto;
 import ru.pcs.crowdfunding.client.services.ClientsService;
-import ru.pcs.crowdfunding.client.services.ProjectsService;
 
 import javax.validation.Valid;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/clients")
@@ -29,7 +26,6 @@ import java.util.stream.Collectors;
 public class ClientController {
 
     private final ClientsService clientsService;
-    private final ProjectsService projectsService;
 
     @GetMapping(value = "/{id}")
     public String getById(@PathVariable Long id, Model model) {
@@ -39,18 +35,9 @@ public class ClientController {
         if (!client.isPresent()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client with id " + id + " not found");
         }
-        List<Project> projects = client.get().getProjects();
-        List<ProjectDto> projectDtos = projects.stream().map(project -> {
-            Optional<ProjectDto> projectDto = projectsService.findById(project.getId());
-            if (!projectDto.isPresent()) {
-                log.error("Project didn't found");
-                throw new IllegalArgumentException("Project didn't found");
-            }
-            return projectDto.get();}).collect(Collectors.toList());
         log.debug("result = {}", client.get());
 
         model.addAttribute("clientDto", client.get());
-        model.addAttribute("projectsDtos", projectDtos);
 
         return "profile_page";
     }
