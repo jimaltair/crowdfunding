35f8b621b32a5b0671b80522031de39c14993153
Mitskevich Igor <jimaltair1987@gmail.com>
2021-12-10 13:50:53 +0200

    add @ToString to Role and Status

--- auth/src/main/java/ru/pcs/crowdfunding/auth/domain/Status.java
+++ auth/src/main/java/ru/pcs/crowdfunding/auth/domain/Status.java
@@ -1,16 +1,13 @@
 package ru.pcs.crowdfunding.auth.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
