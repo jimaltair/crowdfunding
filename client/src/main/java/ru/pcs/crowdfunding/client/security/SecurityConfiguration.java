efe002c8547ae0438151a1661ef4c90b7518b098
Mitskevich Igor <jimaltair1987@gmail.com>
2021-12-10 14:36:16 +0200

    add simple configuration to security, create TokenAuthorizationFilter

--- client/src/main/java/ru/pcs/crowdfunding/client/security/SecurityConfiguration.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/security/SecurityConfiguration.java
@@ -1,19 +1,28 @@
 package ru.pcs.crowdfunding.client.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import ru.pcs.crowdfunding.client.security.filters.TokenAuthorizationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    public static final String API_PING = "/api/v0/ping";
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        TokenAuthorizationFilter tokenAuthorizationFilter = new TokenAuthorizationFilter(objectMapper);
+
         http.csrf().disable();
 
         http.authorizeRequests()
-                .antMatchers(API_PING).permitAll()
+                .antMatchers("**").permitAll()
                 .anyRequest().permitAll();
+
+        http.addFilter(tokenAuthorizationFilter);
     }
 }
---
+++ client/src/main/java/ru/pcs/crowdfunding/client/security/filters/TokenAuthorizationFilter.java
@@ -0,0 +1,24 @@
+package ru.pcs.crowdfunding.client.security.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TokenAuthorizationFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
