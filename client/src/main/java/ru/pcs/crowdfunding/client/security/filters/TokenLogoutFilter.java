7de4a0550f90721086e41f62f5890f473e3eb3d2
Marsel Zakirov <musictest666@yandex.ru>
2021-12-12 22:47:09 +0300

    Убрал логи предупреждения что токена акцесс нет, для страниц (корень, сайн ин и сайн ап).

--- client/src/main/java/ru/pcs/crowdfunding/client/security/filters/TokenLogoutFilter.java
+++ client/src/main/java/ru/pcs/crowdfunding/client/security/filters/TokenLogoutFilter.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ru.pcs.crowdfunding.client.security.SecurityConfiguration;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -22,11 +23,15 @@ public class TokenLogoutFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (logoutRequestMatcher.matches(request)) {
+        if (request.getRequestURI().equals(SecurityConfiguration.SIGN_IN_PAGE) ||
+                request.getRequestURI().equals(SecurityConfiguration.SIGN_UP_PAGE) ||
+                request.getRequestURI().equals(SecurityConfiguration.HOME_PAGE)) {
+            filterChain.doFilter(request, response);
+        }
+        else if (logoutRequestMatcher.matches(request)) {
             SecurityContextHolder.clearContext();
             return;
         }
-        filterChain.doFilter(request, response);
     }
 
 }
