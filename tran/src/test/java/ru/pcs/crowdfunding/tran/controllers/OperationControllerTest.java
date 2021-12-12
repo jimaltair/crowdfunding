70c28d4dacec35a26202bfb65505fc491992d46e
Диана Поварницына <diana.povarnitsina@iqoption.com>
2021-12-12 14:40:27 +0300

    test fix

--- tran/src/test/java/ru/pcs/crowdfunding/tran/controllers/OperationControllerTest.java
+++ tran/src/test/java/ru/pcs/crowdfunding/tran/controllers/OperationControllerTest.java
@@ -8,13 +8,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.pcs.crowdfunding.tran.domain.Account;
+import ru.pcs.crowdfunding.tran.domain.Operation;
 import ru.pcs.crowdfunding.tran.domain.OperationType;
+import ru.pcs.crowdfunding.tran.repositories.AccountsRepository;
 import ru.pcs.crowdfunding.tran.repositories.OperationTypesRepository;
 import ru.pcs.crowdfunding.tran.repositories.OperationsRepository;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -24,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureWebTestClient
 @AutoConfigureTestDatabase
+@DisplayName("OperationController")
 class OperationControllerTest {
 
     @Autowired
@@ -35,40 +41,58 @@ class OperationControllerTest {
     @Autowired
     private OperationTypesRepository operationTypesRepository;
 
+    @Autowired
+    private AccountsRepository accountsRepository;
+
     private String techTranToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6Ik1TX0NMSUVOVCIsInN0YXR1cyI6IkFDVElWRSIsImV4cCI6MjIxNjIzOTAyMn0.Aj-UHmdBosUrf12BrXqn3dsGtXwn0QgBF-q6KP-LvpI";
     private String otherTechTranToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwiZXhwIjoxNjQxNTc5Nzc2fQ.zaAgZjCMUEzML_W-px8al2DQsSOIqemMxDjoRHlQ7MQ";
 
     @BeforeEach
     void setUp() {
-        OperationType testType = OperationType.builder()
+
+
+        //сетишь в БД типы операции
+        operationTypesRepository.save(OperationType.builder()
+                .id(1L)
+                .description("adding funds to your account")
+                .type(OperationType.Type.TOP_UP)
+                .build()
+        );
+        operationTypesRepository.save(OperationType.builder()
                 .id(2L)
                 .description("transfer of funds within the platform")
                 .type(OperationType.Type.PAYMENT)
-                .build();
-//        operationTypesRepository.save(OperationType.builder()
-//                .id(1L)
-//                .description("adding funds to your account")
-//                .type(OperationType.Type.TOP_UP)
-//                .build()
-//        );
-//        operationTypesRepository.save(OperationType.builder()
-//                .id(2L)
-//                .description("transfer of funds within the platform")
-//                .type(OperationType.Type.PAYMENT)
-//                .build()
-//        );
-//        operationTypesRepository.save(OperationType.builder()
-//                .id(3L)
-//                .description("refund within the platform")
-//                .type(OperationType.Type.REFUND)
-//                .build()
-//        );
-//        operationTypesRepository.save(OperationType.builder()
-//                .id(4L)
-//                .description("withdrawal of funds from the platform")
-//                .type(OperationType.Type.WITHDRAW)
-//                .build()
-//        );
+                .build()
+        );
+        operationTypesRepository.save(OperationType.builder()
+                .id(3L)
+                .description("refund within the platform")
+                .type(OperationType.Type.REFUND)
+                .build()
+        );
+        operationTypesRepository.save(OperationType.builder()
+                .id(4L)
+                .description("withdrawal of funds from the platform")
+                .type(OperationType.Type.WITHDRAW)
+                .build()
+        );
+
+        accountsRepository.save(Account.builder()
+            .isActive(true)
+            .id(1l)
+            .createdAt(Instant.parse("2017-02-03T11:25:30.00Z"))
+            .modifiedAt(Instant.parse("2018-02-03T11:25:30.00Z"))
+            .build());
+
+
+        OperationType testType = OperationType.builder()
+            .id(2L)
+            .description("transfer of funds within the platform")
+            .type(OperationType.Type.PAYMENT)
+            .build();
+
+
+
 //        operationsRepository.save(Operation.builder()
 //                .operationType(OperationType.builder().type(testType.getType()).build())
 //                .initiator(1l)
@@ -88,6 +112,9 @@ class OperationControllerTest {
 //                .id(1l)
 //                .sum(BigDecimal.valueOf(500))
 //                .build());
+//
+//
+//
 //        operationsRepository.save(Operation.builder()
 //                .operationType(OperationType.builder().type(testType.getType()).build())
 //                .initiator(2l)
@@ -118,7 +145,7 @@ class OperationControllerTest {
             mockMvc.perform(post("/api/operation")
                             .contentType(MediaType.APPLICATION_JSON)
                             .header("Authorization", techTranToken)
-                            .content("{\"initiatorId\": 1, \"operationType\": \"TOP_UP\", \"debitAccountId\": \"1\", \"sum_payment\": \"200.00\"}")
+                            .content("{\"initiatorId\": 1, \"operationType\": \"TOP_UP\", \"debitAccountId\": \"1\", \"sum\": \"200.00\"}")
                     )
                     .andDo(print())
                     .andExpect(status().is2xxSuccessful())
